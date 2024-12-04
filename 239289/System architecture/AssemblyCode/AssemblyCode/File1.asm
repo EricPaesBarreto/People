a88477df59b4e688d72a1e619e14de457820e2bf
Eric Paes Barreto <ericpaesbarreto@outlook.com>
2024-12-04 11:18:11 +0100

    Did some asm

--- 239289/System architecture/AssemblyCode/AssemblyCode/File1.asm
+++ 239289/System architecture/AssemblyCode/AssemblyCode/File1.asm
@@ -1,43 +1,19 @@
 INCLUDE Irvine32.inc
 .386
 .data
-val1 WORD 1000h
-val2 WORD 2000h
-arrayB BYTE 10h, 20h, 30h, 40h, 50h
-arrayW WORD 100h, 200h, 300h
-arrayD DWORD 10000h, 20000h
+	arrayB BYTE 3h, 1h, 4h, 1h, 5h	; Create array (PI)
 .code
 main PROC
-;MOVZX
-mov bx, 0A69Bh ; Initialize BX reg
-movzx eax, bx ; EAX = TODO
-movzx edx, bl ; EDX = TODO
-movzx cx, bl ; CX = TODO
-Call DumpRegs
-; MOVSX
-mov bx, 0A69Bh ; Initialize BX reg
-movsx eax, bx ; EAX = TODO
-movsx edx, bl ; EDX = TODO
-movsx cx, bl ; CX = TODO
-call DumpRegs
-; Memory-to-memory exchange
-mov ax, val1 ; AX = TODO
-xchg ax, val2 ; AX = TODO,val2 = TODO
-mov val1, ax ; val1 = TODO
-call DumpRegs
-; Direct-offset Addressing (byte array)
-mov al, arrayB ; AL = TODO
-mov al, [arrayB+1] ; AL = TODO
-mov al, [arrayB+2] ; AL = TODO
-call DumpRegs
-; Direct-offset Addressing (word array)
-mov ax, arrayW ; AX = TODO
-mov ax, [arrayW+2] ; AX = TODO
-call DumpRegs
-; Direct-offset Addressing (doubleword array)
-mov eax, arrayD ; EAX = TODO
-mov eax, [arrayD+4] ; EAX = TODO
-call DumpRegs
-ret
+	mov ECX, SIZEOF arrayB		; Store in ECX array length
+	mov ESI, OFFSET arrayB		; Store 0 (index) in ESI
+	mov CL, 0h
+
+	beginning:					; While ECX > 0
+		mov BL, arrayB[ESI];	; Store the value of arrayB at position ESI in BX reg
+		add CL, BL				; Add current value (BL reg) to sum (CL reg)
+		inc ESI					; Increment ESI
+	LOOP beginning				; GOTO beginning of loop, decrement ECX
+
+	call DumpRegs
 main ENDP
 END main
